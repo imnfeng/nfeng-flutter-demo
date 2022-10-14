930d463a396facf255e54b2b20f98ec42a8b62c2
N-feng <308561157@qq.com>
2022-10-14 19:08:42 +0800

    feat: ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,147 @@
 import 'package:flutter/material.dart';
-import './nfengFont.dart';
+import 'package:flutter01/nfengFont.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: const Text("你好Flutter")),
-      body: Column(
-        children: const [
-          MyApp(),
-          MyButton(),
-          // MyText(),
-          SizedBox(height: 20),
-          Circular(),
-          Icon(
-            NfengIcon.weixin,
-            size: 40,
-            color: Colors.orange,
-          )
-          // ClipImage(),
-          // LocalImage()
-        ],
-      ),
-    ),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-        alignment: Alignment.center,
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          border: Border.all(color: Colors.red, width: 2),
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [BoxShadow(color: Colors.blue, blurRadius: 20.0)],
-          gradient: const LinearGradient(colors: [Colors.red, Colors.yellow]),
-        ),
-        child: const Text(
-          "你好Flutter",
-          style: TextStyle(color: Colors.black, fontSize: 20),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Flutter ICON")),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // height: 120,
+      child: ListView(
+        // scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(10),
+        children: <Widget>[
+          // Container(
+          //   width: 120,
+          //   decoration: const BoxDecoration(color: Colors.red),
+          //   child: Column(children: [
+          //     SizedBox(
+          //       width: 80,
+          //       child: Image.network(
+          //         "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg",
+          //         fit: BoxFit.cover,
+          //       ),
+          //     ),
+          //     const Text('文字')
+          //   ]),
+          // ),
+
+          ListTile(
+            leading: Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg",
+            ),
+            title: const Text("新闻标题"),
+            subtitle: const Text("新闻内容"),
+          ),
+          const Divider(),
+          ListTile(
+            leading: Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg",
+            ),
+            title: const Text("新闻标题"),
+            subtitle: const Text("新闻内容"),
+          ),
+          const Divider(),
+          ListTile(
+            trailing: Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg",
+            ),
+            title: const Text("新闻标题"),
+            subtitle: const Text("新闻内容"),
+          ),
+          const Divider(),
+          ListTile(
+            trailing: Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg",
+            ),
+            title: const Text("新闻标题"),
+            subtitle: const Text("新闻内容"),
+          ),
+          const Divider(),
+          Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg"),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+            height: 44,
+            child: const Text(
+              "我是一个标题",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+          Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg"),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+            height: 44,
+            child: const Text(
+              "我是一个标题",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+          Image.network(
+              "https://feng-1257981287.cos.ap-chengdu.myqcloud.com/uPic/z78vVm.jpg"),
+          const Divider(),
+          const ListTile(
+            leading: Icon(
+              NfengIcon.weixin,
+              color: Colors.green,
+            ),
+            title: Text("微信登录"),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.assessment),
+            title: Text("全部订单"),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(
+              Icons.payment,
+              color: Colors.green,
+            ),
+            title: Text("待付款"),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(
+              Icons.favorite,
+              color: Colors.lightGreen,
+            ),
+            title: Text("我的收藏"),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(
+              Icons.people,
+              color: Colors.black54,
+            ),
+            title: Text("在线客服"),
+            trailing: Icon(Icons.chevron_right_sharp),
+          ),
+        ],
       ),
     );
   }
